4a3eab0c34202885c79d5323257c22143118f44c
William Li <wli@objectivasoftware.com>
2013-12-20 11:42:44 +0800

    initial

---
+++ ruby.rb
@@ -0,0 +1,66 @@
+#  What is Ruby
+#    - Ruby was created by Yukihiro Matsumoto(ËÉ±¾ÐÐºë, 1965/4/14-?) in 1993
+#    - It's a scripting language
+#      -- Interpredted
+#      -- Dynamic
+#      -- Weakly Typed
+#    - Pure Object-Oriented
+   
+   
+#  Chapter 1. Everything is an Object
+#    - Literals
+#      -- String
+###############
+"Hello, Ruby" #
+'Hello, Ruby' #
+###############
+
+
+#     -- Number
+##########################################
+1                                        #
+1.0                                      #
+1.5                                      #
+9999999999999999999999999999999999999999 #
+1.3e4 # 1.3x10^4                         #
+##########################################                 
+
+
+#      -- Array
+###############################
+[1, 2, 3]                     #
+[[1,2], [3, 4], [5, 6]]       #
+[1, "string", 3, [4, "five"]] #
+###############################
+
+
+#      -- Hash
+####################
+{                  #
+  "a" => "b",      #
+  "foo" => "bar"   #
+}                  #
+                   #
+{                  #
+  :a => "b",       #
+  :foo => "bar"    #
+}                  #
+####################
+
+#    - Method
+a = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
